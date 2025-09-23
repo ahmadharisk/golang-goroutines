b1cf1470f54735461295b70531e443a150b2c5a7
Peppo <ahmadharisk@gmail.com>
2025-09-23 20:41:25 +0700

    hari 14, belajar penggunaan mutex untuk mengatasi race condition

---
+++ mutex_test.go
@@ -0,0 +1,25 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestMutex(t *testing.T) {
+	x := 0
+	var mutex sync.Mutex
+	for i := 0; i < 1000; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				mutex.Lock()
+				x++
+				mutex.Unlock()
+			}
+		}()
+	}
+
+	time.Sleep(time.Duration(5) * time.Second)
+	fmt.Println(x)
+}
