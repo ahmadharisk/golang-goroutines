3ac5dac2694fb66ba4ffef370143bc44c0402b2b
Peppo <ahmadharisk@gmail.com>
2025-09-28 11:31:57 +0700

    hari 17, belajar penggunaan sync cond, locking berbasis kondisi

---
+++ cond_test.go
@@ -0,0 +1,49 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+var locker = sync.Mutex{}
+var cond = sync.NewCond(&locker)
+var group = sync.WaitGroup{}
+
+func WaitCondition(value int) {
+	defer group.Done()
+	group.Add(1)
+	// 1) membuat kondisi lock
+	cond.L.Lock()
+
+	// 2) kemudiam membuat
+	cond.Wait()
+
+	fmt.Println("selesai", value)
+
+	// 3) di unlock
+	cond.L.Unlock()
+}
+
+func TestCond(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go WaitCondition(i)
+	}
+
+	//go func() {
+	//	for i := 0; i < 10; i++ {
+	//		time.Sleep(1 * time.Second)
+	//		cond.Signal()
+	//	}
+	//}()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			time.Sleep(1 * time.Second)
+			cond.Broadcast()
+		}
+	}()
+
+	group.Wait()
+}
